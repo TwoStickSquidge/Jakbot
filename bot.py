36a3b0e066273b8162834345c9bb974c7a3442d4
Squidge <eclarkmaracle@gmail.com>
2024-04-30 09:37:52 -0400

    Delete token.txt

---
+++ bot.py
@@ -0,0 +1,72 @@
+import discord
+from discord.ext import commands
+import os, shutil
+
+import random
+
+queue = []
+
+image_names = []
+download_folder = "downloads"
+submission_folder = "forReview"
+submission_names = []
+
+def update_images():
+        for filename in os.listdir(download_folder):
+            image_names.append(os.path.join(download_folder, filename))
+
+update_images()
+
+intents = discord.Intents.all()
+
+Bot = commands.Bot(command_prefix='!',intents=intents) 
+
+@Bot.command()
+async def soy(ctx, *args, **kwargs):
+        if (len(image_names) > 0):
+            random_image = random.choice(image_names)
+            img = random_image
+        else:
+              img = image_names[0]
+
+        await ctx.send(file=discord.File(img))
+
+@Bot.command()
+async def submitsoy(ctx, *args, **kwargs):
+
+    if(len(ctx.message.attachments) > 0):
+        i = 0
+        while i < len(ctx.message.attachments):
+            filename = ctx.message.attachments[i].filename
+    
+            if (ctx.message.attachments[i].content_type[0:5] == "image"):
+                await ctx.message.attachments[i].save(os.path.join(submission_folder, filename))
+                await ctx.send("Soyjak submitted!")
+            else:
+                await ctx.send("File type not supported: " + ctx.message.attachments[i].content_type)
+            i += 1
+    else:
+        await ctx.send("No file attached.")
+
+@Bot.command()
+async def updatesoy(ctx, *args, **kwargs):
+      image_names = []
+      update_images()
+      await ctx.send("Soys Updated!")
+
+@Bot.command()
+async def approvesoys(ctx, *args, **kwargs):
+    if(ctx.author.name == "twosticksquidge"):
+        for filename in os.listdir(submission_folder):
+            shutil.move(os.path.join(submission_folder, filename), os.path.join(download_folder, filename))
+        update_images()
+        await ctx.send("Soys approved!")
+    else:
+        await ctx.send("You do not have permission to use that command")
+          
+
+token = ""
+with open("token.txt") as file:
+    token = file.read()
+
+Bot.run(token)
